f107b392dde7da1f08ff78d30871c6e258ff3adf
Bloin <Bolin8621@gmail.com>
2024-08-01 00:07:18 +0800

    Improved the visuals of pick up parts.
    Improved the visuals of pick up parts.
    Also improved RocketPart "prefab", with exported live variables editable on the rocket part itself.

--- progress_3d/rocket_part.gd
+++ progress_3d/rocket_part.gd
@@ -1,7 +1,8 @@
+@tool
 class_name RocketPart
 extends CharacterBody3D
 
-@export var target: Node3D
+var target: ShipProgressTarget
 
 var picked_up = false
 
@@ -13,43 +14,144 @@ var follow_time: float = 0.05  # Time to reach target during normal following
 var current_move_time: float
 var pickup_timer: float = 0.0
 
+var has_reached_target: bool = false
+var pos_at_pickup: Vector3
+
+static var parts_count: int = 0
+static var remaining_parts_count: int = 0
+
+@export var pick_sphere_radius: float = 2.0:
+	set(v):
+		pick_sphere_radius = v
+		var ps: MeshInstance3D = get_node("PickSphere")
+		ps.mesh = ps.mesh.duplicate() # make unique
+		var sphere: SphereMesh = ps.get_mesh()
+		sphere.radius = v
+		sphere.height = 2 * v
+@export var pick_sphere_color: Color = Color('0000ff61'):
+	set(v):
+		pick_sphere_color = v
+		var ps: MeshInstance3D = get_node("PickSphere")
+		var sphere: SphereMesh = ps.get_mesh()
+		var mat: StandardMaterial3D = sphere.get_material()
+		mat.albedo_color = v
+@export var pick_sphere_y: float = 0:
+	set(v):
+		pick_sphere_y = v
+		var ps: MeshInstance3D = get_node("PickSphere")
+		ps.position.y = v
+@export var show_bbox: bool = true:
+	set(v):
+		show_bbox = v
+		var bbox = get_node("BBox")
+		bbox.set_visible(v)
+
+@onready var pick_sphere: MeshInstance3D = $PickSphere
+@onready var b_box: Node3D = $BBox
+@onready var illusion_body: CharacterBody3D = $IllusionBody
+@onready var illusion_mesh: MeshInstance3D = $IllusionBody/IllusionMesh
+
+var model: MeshInstance3D
+var collision_shape: CollisionShape3D
+
+
+func _ready():
+	for c in get_children():
+		if not c is MeshInstance3D:
+			continue
+		elif c in [pick_sphere, b_box]:
+			continue
+		else:
+			model = c
+			break
+	assert(model)
+	assert(pick_sphere)
+	
+	if not Global.ship_progress_target.is_node_ready():
+		await Global.ship_progress_target.is_node_ready
+	
+	target = Global.ship_progress_target
+	
+	pick_sphere.create_trimesh_collision()
+	var static_body = pick_sphere.get_child(0)
+	var collision_shape = static_body.get_child(0)
+	collision_shape.reparent(self)
+	pick_sphere.remove_child(static_body)
+	static_body.queue_free()
+	
+	b_box.hide()
+	
+	# this part needs updating when we use proper models
+	var dupe_mesh = model.mesh.duplicate()
+	var mat = StandardMaterial3D.new()
+	# Transparency TRANSPARENCY_ALPHA = 1
+	mat.set_transparency(1)
+	mat.albedo_color = Color(Color.DARK_BLUE, 0.75)
+	dupe_mesh.set_material(mat)
+	illusion_mesh.mesh = dupe_mesh
+	illusion_mesh.set_position(model.get_position())
+	
+	parts_count += 1
+	remaining_parts_count += 1
 
 func _physics_process(delta):
-	if not picked_up:
+	if Engine.is_editor_hint():
 		return
-	if target:
+	
+	# var to_move: CharacterBody3D = self if picked_up else illusion_body
+	var to_moves = []
+	if picked_up:
+		to_moves.append(self)
+	if not has_reached_target:
+		to_moves.append(illusion_body)
+	if not to_moves:
+		return
+	if picked_up:
 		pickup_timer += delta
-		
-		target_position = target.global_position
-		target_rotation = target.get_quaternion()
+	
+	target_position = target.global_position
+	target_rotation = target.get_quaternion()
+	
+	for to_move in to_moves:
+		if to_move == self:
+			if not has_reached_target:
+				# During initial pickup movement
+				var t = pickup_timer / initial_pickup_time
+				to_move.global_position = pos_at_pickup.lerp(target_position, t)
+				var dist = target_position.distance_to(to_move.global_position)
+				if dist < 0.1:
+					pick_up_reached()
+			else:
+				var direction = (target_position - global_position)
+				to_move.velocity = direction / follow_time
+				to_move.move_and_slide()
+		elif to_move == illusion_body:
+			var direction = (target_position - to_move.global_position)
+			to_move.velocity = direction / follow_time
+			to_move.move_and_slide()
+			
+		to_move.global_rotation = target.global_rotation
 
-		current_move_time = initial_pickup_time \
-			if pickup_timer <= initial_pickup_time \
-			else follow_time
 
-		var direction = (target_position - global_position)
-		velocity = direction / current_move_time
-
-		move_and_slide()
-
-		global_rotation = target.global_rotation
-		
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		var camera3d = get_viewport().get_camera_3d()
-		var from = camera3d.project_ray_origin(event.position)
-		var to = from + camera3d.project_ray_normal(event.position) * 1000
-
-		var space_state = get_world_3d().direct_space_state
-		var query = PhysicsRayQueryParameters3D.create(from, to)
-		var result = space_state.intersect_ray(query)
-
-		if result and result.collider == self:
-			pick_up()
+	if Engine.is_editor_hint():
+		return
 
 func pick_up():
 	picked_up = true
 	set_collision_layer(0)
 	set_collision_mask(0)
 	pickup_timer = 0.0
-	get_node("PickSphere").hide()
+	pos_at_pickup = global_position
+	pick_sphere.hide()
+
+func pick_up_reached():
+	has_reached_target = true
+	for c in illusion_body.get_children():
+		illusion_body.remove_child(c)
+		c.queue_free()
+	remove_child(illusion_body)
+	illusion_body.queue_free()
+	remaining_parts_count -= 1
+	if remaining_parts_count == 0:
+		Global.all_parts_picked_up.emit()
--- progress_3d/ship_progress_target.gd
+++ progress_3d/ship_progress_target.gd
@@ -1,17 +1,21 @@
+class_name ShipProgressTarget
 extends Marker3D
 
-@export var camera: Camera3D
-@export var viewport_height_percentage = 0.4
+var camera: Camera3D
+@export var viewport_height_percentage = 0.5
 
 # 8 by 4
-# 3 padding on each side
-@export var object_height = 14.0
-@export var object_width = 10.0
+# 2 padding on each side
+@export var object_height = 12.0
+@export var object_width = 8.0
 
 @onready var v_stretch = $Control/VBoxContainer/Stretch
 @onready var bg_panel = $Control/VBoxContainer/Panel
 
+
 func _ready():
+	camera = get_viewport().get_camera_3d()
+	
 	v_stretch.set_stretch_ratio(1.0 - viewport_height_percentage)
 	bg_panel.set_stretch_ratio(viewport_height_percentage)
 	await get_tree().process_frame 
@@ -19,8 +23,12 @@ func _ready():
 	var size = bg_rect.size
 	size.x = size.y / object_height * object_width
 	bg_panel.set_custom_minimum_size(size)
+	
+	sync_with_camera()
+	
+	Global.all_parts_picked_up.connect(all_parts_picked_up)
 
-func _physics_process(delta):
+func sync_with_camera():
 	var viewport_size = get_viewport().size
 	
 	var desired_height_pixels = viewport_size.y * viewport_height_percentage
@@ -44,15 +52,11 @@ func _physics_process(delta):
 	var world_position = camera.global_transform.origin + position_offset
 	
 	global_transform.origin = world_position
-	
-	#var to_camera = camera.global_transform.origin - global_transform.origin
-#
-	## Create a basis that looks at the camera
-	#var look_at_basis = Basis.looking_at(to_camera, Vector3.UP)
-#
-	## Interpolate between the current orientation and the look-at orientation
-	#var rotation_factor = 0.5  # Adjust this value between 0 and 1 to control how much it faces the camera
-	#var final_basis = global_transform.basis.slerp(look_at_basis, rotation_factor)
-#
-	## Apply the new orientation
-	#global_transform.basis = final_basis
+
+func all_parts_picked_up():
+	var theme: Theme = bg_panel.get_theme().duplicate()
+	# Godot wttf is this theme.get_stylebox syntax
+	var sb_flat: StyleBoxFlat = theme.get_stylebox("panel", "Panel")
+	var alpha = sb_flat.border_color.a
+	sb_flat.border_color = Color(Color.LIME_GREEN, alpha)
+	bg_panel.set_theme(theme)
