8e18f564b64b0fe40823d3ab5e391937f7bbec98
Bloin <Bolin8621@gmail.com>
2024-08-02 00:22:44 +0800

    Added pick up system.
    Added pick up system that works with camera.

--- misc/global.gd
+++ misc/global.gd
@@ -7,8 +7,13 @@ signal victory
 
 signal all_parts_picked_up
 
+var in_game: bool = false
+
 func _ready():
-	pass
+	start_game.connect(func(): in_game = true)
+	restart.connect(func(): in_game = true)
+	victory.connect(func(): in_game = false)
+	to_main_menu.connect(func(): in_game = false)
 
 func _input(event):
 	if OS.is_debug_build():
--- player/player_camera.gd
+++ player/player_camera.gd
@@ -1,9 +1,19 @@
+@tool
 class_name PlayerCamera
 extends Node3D
 
 @onready var yaw_node = $CamYaw
 @onready var pitch_node = $CamYaw/CamPitch
 @onready var camera: Camera3D = $CamYaw/CamPitch/SpringArm/PlayerCamera
+@onready var interact_raycast: RayCast3D = $CamYaw/CamPitch/SpringArm/PlayerCamera/InteractRaycast
+
+@export var y_offset: float = 2.0:
+	set(v):
+		y_offset = v
+		var cam_yaw: Node3D = get_node_or_null("CamYaw")
+		if not cam_yaw:
+			return
+		cam_yaw.position = Vector3(0, y_offset, 0)
 
 var yaw : float = 0
 var pitch : float = 0
@@ -16,8 +26,10 @@ static var pitch_sensitivity: float = 0.1
 var yaw_acceleration: float = 20
 var pitch_acceleration: float = 20
 
-var pitch_max: float = 25
+var pitch_max: float = 40
 var pitch_min: float = -65
+
+var current_interacter: Node3D
 
 func _ready():
 	instance = self
@@ -28,16 +40,34 @@ func start_game():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
+	if Engine.is_editor_hint():
+		return
+	
 	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
 		return
 	if event is InputEventMouseMotion:
-		yaw += event.relative.x * yaw_sensitivity
-		pitch += event.relative.y * pitch_sensitivity
+		yaw -= event.relative.x * yaw_sensitivity
+		pitch -= event.relative.y * pitch_sensitivity
 
 func _physics_process(delta):
+	if Engine.is_editor_hint():
+		return
+	
 	pitch = clamp(pitch, pitch_min, pitch_max)
 	
 	yaw_node.rotation_degrees.y = lerp(
 		yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
 	pitch_node.rotation_degrees.x = lerp(
 		pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+
+	var collider = null
+	if interact_raycast.is_colliding():
+		collider = interact_raycast.get_collider()
+	if collider == current_interacter:
+		return
+	# collider !=  current_interacter
+	if current_interacter and current_interacter.has_method("hide_interact_hint"):
+		current_interacter.hide_interact_hint()
+	current_interacter = collider
+	if collider and collider.has_method("show_interact_hint"):
+		collider.show_interact_hint()
